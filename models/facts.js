be719a542998bdf9e785f764302c97d5514b085d
Vianney-Gln <vgeloen@gmail.com>
2022-02-02 15:32:44 +0100

    modif model getFacts , mise en place d'inner join pour joindre category et facts

--- models/facts.js
+++ models/facts.js
@@ -4,7 +4,11 @@ const db = connection.promise();
 //function gettting facts from db
 
 const getFacts = () => {
-  return db.query("SELECT * FROM facts").then((result) => result[0]);
+  return db
+    .query(
+      "SELECT * FROM facts INNER JOIN category ON facts.id_category = category.id"
+    )
+    .then((result) => result[0]);
 };
 
 //function gettting one fact from db by his Id
